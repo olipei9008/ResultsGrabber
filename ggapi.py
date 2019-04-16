2dba3696fe4303c413ce424f0d0964e6c0773985
Oliver Pei <vespestas@gmail.com>
2019-04-16 11:06:57 -0700

    Added comments + added security

--- ggapi.py
+++ ggapi.py
@@ -1,16 +1,27 @@
 import requests
 
+# Class to interact with the smash.gg API
 class GGApi:
+
+    # For now, only take in dates for getting tournaments in SoCal within the period.
     def __init__(self, afterDate, beforeDate):
         self.afterDate = afterDate
         self.beforeDate = beforeDate
 
+
+    # Making a query and sending it to smash gg
     def send_query(self):
-        headers = {"Authorization": "Bearer 108f713093eebd8a3153f295565ae22f"}
+
+        # need to edit in auth token before running
+        headers = {"Authorization": "Bearer insert auth token"}
+
+        # Hardcoded values to find tournaments for SoCal Melee
         perPage = 100
         coordinates = "33.7454725,-117.86765300000002"
         radius = "80mi"
-        vgID = 1
+        vgID = 1 # SSBM
+
+        # Using very inelegant string concatenation to create the query + variables
         query = "query SocalTournaments" \
                 "($perPage: Int, $coordinates: String!, $radius: String!, $videogameId: ID!, $beforeDate: Timestamp!, $afterDate: Timestamp!) " \
                 "{ tournaments(query: { perPage: $perPage filter: " \
@@ -20,13 +31,14 @@ class GGApi:
                 "{ nodes { id name city url startAt } } }"
         variables = "{ \"perPage\": "+str(perPage)+", \"coordinates\": \""+coordinates+"\", " \
                 "\"radius\": \""+radius+"\", \"videogameId\": "+str(vgID)+", \"afterDate\": 1545033600, \"beforeDate\": 1555920000 }"
+
+        # Finishing up setting up the variables
         url = "https://api.smash.gg/gql/alpha"
         data = {
             "query": query,
             "variables": variables
         }
-        print(url)
-        print(headers)
-        print(data)
+
+        # Send!
         r = requests.post(url=url, headers=headers, json=data)
         return r
--- results_grabber.py
+++ results_grabber.py
@@ -2,11 +2,17 @@ from ggapi import *
 import csv
 
 def main():
+
+    # Hardcoded timestamps
     afterDate = 1545033600
     beforeDate = 1555397935
+
+    # Getting the values
     gg_runner = GGApi(afterDate, beforeDate)
     results = gg_runner.send_query()
     tournament_data = results.json()['data']['tournaments']['nodes']
+
+    # For each entry, do stuff
     for tourney in tournament_data:
         print(tourney['url'] + str(tourney['startAt']))
 
